8fe8f9cc33377824aaefadcfda9c7f72ed9d1dcc
Titan Natesan <143887015+TitanNatesan@users.noreply.github.com>
2024-10-31 22:13:15 +0530

    new UI started

--- AslamGarmentsB/AslamGarmentsB/settings.py
+++ AslamGarmentsB/AslamGarmentsB/settings.py
@@ -30,10 +30,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
-
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_HEADERS = (
---
+++ aslam-graments-f/src/app/login/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const mainPage = () => {
+    
+    return (
+        <div>
+            <h1>Hello Wrold</h1>
+        </div>
+    )
+}
+
+export default mainPage;
